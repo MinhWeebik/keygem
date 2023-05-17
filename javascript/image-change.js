fa5db1ca493650f0e1610d0fb1d7b1d7cad3fb2d
MinhWeebik <banana2252003@gmail.com>
2023-05-17 10:58:16 +0700

    update

--- javascript/image-change.js
+++ javascript/image-change.js
@@ -24,29 +24,73 @@ function showImage(n)
 
 }
 
-document.getElementById('display1').onclick = () =>
+function imageZoom(imgID,lensID)
 {
-  document.getElementById('popup-1').style.display = 'block';
-};
-document.getElementById('display2').onclick = () =>
-{
-  document.getElementById('popup-2').style.display = 'block';
-};
-document.getElementById('display3').onclick = () =>
-{
-  document.getElementById('popup-3').style.display = 'block';
-};
-document.getElementById('display4').onclick = () =>
-{
-  document.getElementById('popup-4').style.display = 'block';
-};
-
-document.querySelectorAll('.pop-up-image .close-popup').forEach(span =>
+  let img = document.getElementById(imgID);
+  let lens = document.getElementById(lensID);
+  lens.style.backgroundImage = `url(${img.src})`
+  let ratio = 3;
+  lens.style.backgroundSize = (img.width * ratio) + 'px ' + (img.height * ratio) + 'px';
+  img.addEventListener('mousemove', moveLens);
+  lens.addEventListener('mousemove',moveLens);
+  function getCursor()
   {
-    span.onclick = () => {
-      document.getElementById('popup-1').style.display = 'none';
-      document.getElementById('popup-2').style.display = 'none';
-      document.getElementById('popup-3').style.display = 'none';
-      document.getElementById('popup-4').style.display = 'none';
+    let e = window.event;
+    let bounds = img.getBoundingClientRect();
+    let y = e.pageY - bounds.top;
+    y -= window.pageYOffset;
+    let x = e.pageX - bounds.left;
+    x -= window.pageXOffset;
+    return {'x':x, 'y':y};
+  }
+  function moveLens()
+  {
+    let pos = getCursor()
+    let positionLeft = pos.x - (lens.offsetWidth / 2);
+    let positionTop = pos.y - (lens.offsetHeight / 2);
+    if(positionLeft < 0)
+    {
+      positionLeft = 0;
     }
-  })
+    if(positionTop < 0)
+    {
+      positionTop = 0;
+    }
+    if(positionLeft > img.width - lens.offsetWidth)
+    {
+      positionLeft = img.width - lens.offsetWidth;
+    }
+    if(positionTop > img.height - lens.offsetHeight)
+    {
+      positionTop = img.height - lens.offsetHeight;
+    }
+    lens.style.left = positionLeft + 'px';
+    lens.style.top = positionTop + 'px';
+    lens.style.backgroundPosition = '-' + ((pos.x-80) * ratio) + 'px -' + ((pos.y-80) * ratio) + 'px'
+    function leaveLens()
+    {
+      lens.classList.remove('active');
+      console.log('etst');
+    }
+    lens.addEventListener('mouseout', leaveLens);
+    lens.classList.add('active');
+  }
+}
+
+document.getElementById('display1').addEventListener('mouseover',()=>
+{
+  imageZoom('display1','lens1');
+})
+document.getElementById('display2').addEventListener('mouseover',()=>
+{
+  imageZoom('display2','lens2');
+})
+document.getElementById('display3').addEventListener('mouseover',()=>
+{
+  imageZoom('display3','lens3');
+})
+document.getElementById('display4').addEventListener('mouseover',()=>
+{
+  imageZoom('display4','lens4');
+})
+
